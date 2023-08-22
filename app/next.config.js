c4e85db624fc21f6c212aa00119137bc8d386a52
Mitchell Barton <mitchell@welikesmall.com>
2023-08-22 09:50:19 -0600

    updating images issue

--- app/next.config.js
+++ app/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['dirty-snow-panel.local.com', 'dirty-snow-admin.hot-rice.com'],
+    domains: ['dirty-snow-panel.local.com', 'dirty-snow-admin.hot-rice.com', 'admin-dirty-snow.warm-rice.dev'],
   },
 }
 
---
+++ cms/site/plugins/pluginkit-4-panel/src/components/DemoSection.vue
@@ -0,0 +1,19 @@
+<template>
+	<button class="build-button">Build Site</button>
+</template>
+
+<script>
+export default {
+	name: 'DemoSection',
+	mounted() {
+		console.log('hello')
+	}
+};
+</script>
+
+<style>
+/** Put your CSS here **/
+.build-button {
+	background: red;
+}
+</style>
